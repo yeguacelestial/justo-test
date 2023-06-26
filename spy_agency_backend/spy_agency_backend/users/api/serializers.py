49f63f4cc2f8adf83944cf57c5055c925729593d
Carlos Nava <yeguacelestial@gmail.com>
2023-06-25 19:39:56 -0600

    Add custom fields for user serialzier

--- spy_agency_backend/spy_agency_backend/users/api/serializers.py
+++ spy_agency_backend/spy_agency_backend/users/api/serializers.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -5,9 +6,14 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    _type = serializers.ChoiceField(choices=User.Types.choices)
+    description = serializers.CharField()
+
     class Meta:
         model = User
-        fields = ["name", "url"]
+        fields = ["name", "url", "email", "password", "_type", "description"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
