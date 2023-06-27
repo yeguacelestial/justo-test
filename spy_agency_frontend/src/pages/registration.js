92148f4bad8e154da28f19dee940e10d29b2c5fc
Carlos Nava <yeguacelestial@gmail.com>
2023-06-27 01:16:58 -0600

    Create login page

---
+++ spy_agency_frontend/src/pages/registration.js
@@ -0,0 +1,10 @@
+"use client"
+export default function Registration() {
+    return (
+        <html class="h-full bg-black">
+            <body class="h-full">
+                Registration
+            </body>
+        </html>
+    )
+}
