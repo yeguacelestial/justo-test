e24d0ce3257fca2a2fe3ef1363ec009102075746
Carlos Nava <yeguacelestial@gmail.com>
2023-06-29 23:50:07 -0600

    Fix background bug

--- spy_agency_frontend/src/components/InChargeOfModal.js
+++ spy_agency_frontend/src/components/InChargeOfModal.js
@@ -1,15 +1,45 @@
-"use client"
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
-import { Fragment, useRef, useState } from "react"
+const people = [
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    { name: "Lindsay Walton", title: "Front-end Developer", email: "lindsay.walton@example.com", role: "Member" },
+    // More people...
+];
 
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
 
 export default function InChargeOfModal({ hitmanId, isOpen, closeModal }) {
     const cancelButtonRef = useRef(null);
 
+    const [modalOpen, setModalOpen] = useState(isOpen);
+
+    useEffect(() => {
+        setModalOpen(isOpen)
+    }, [isOpen])
+
+    const handleOutsideClick = (event) => {
+        if (event.target === event.currentTarget) {
+            closeModal();
+        }
+    };
+
     return (
-        <Transition.Root show={isOpen} as={Fragment}>
+        <Transition.Root show={modalOpen} as={Fragment}>
             <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={closeModal}>
                 <Transition.Child
                     as={Fragment}
@@ -23,46 +53,96 @@ export default function InChargeOfModal({ hitmanId, isOpen, closeModal }) {
                     <div className="backdrop-blur-sm fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                 </Transition.Child>
 
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-                                <div>
-                                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                                        <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                                    </div>
-                                    <div className="mt-3 text-center sm:mt-5">
-                                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                                            Payment successful
-                                        </Dialog.Title>
-                                        <div className="mt-2">
-                                            <p className="text-sm text-gray-500">
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="mt-5 sm:mt-6">
-                                    <button
-                                        type="button"
-                                        className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                        onClick={closeModal}
-                                    >
-                                        Go back to dashboard
-                                    </button>
-                                </div>
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
+                <div onClick={handleOutsideClick} className="fixed inset-0 flex items-center justify-center">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <div className="relative max-w-3xl w-full bg-white shadow-xl rounded-lg p-4 sm:p-6">
+                            <div className="overflow-x-auto lg:overflow-x-hidden max-h-[20rem] mt-8">
+                                <table className="min-w-full border-separate border-spacing-0">
+                                    <thead className="sticky top-0 bg-white bg-opacity-75 z-10">
+                                        <tr>
+                                            <th
+                                                scope="col"
+                                                className="border-b border-gray-300 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                                            >
+                                                Name
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="hidden border-b border-gray-300 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                                            >
+                                                Title
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="hidden border-b border-gray-300 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                            >
+                                                Email
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="border-b border-gray-300 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                            >
+                                                Role
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="border-b border-gray-300 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                                            >
+                                                <span className="sr-only">Edit</span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="divide-y divide-gray-200">
+                                        {people.map((person, personIdx) => (
+                                            <tr key={person.email}>
+                                                <td
+                                                    className={classNames(
+                                                        "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8"
+                                                    )}
+                                                >
+                                                    {person.name}
+                                                </td>
+                                                <td
+                                                    className={classNames(
+                                                        "hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell"
+                                                    )}
+                                                >
+                                                    {person.title}
+                                                </td>
+                                                <td
+                                                    className={classNames(
+                                                        "hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell"
+                                                    )}
+                                                >
+                                                    {person.email}
+                                                </td>
+                                                <td className={classNames("whitespace-nowrap px-3 py-4 text-sm text-gray-500")}>
+                                                    {person.role}
+                                                </td>
+                                                <td
+                                                    className={classNames("relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium")}
+                                                >
+                                                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                                        Edit<span className="sr-only">, {person.name}</span>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </Transition.Child>
                 </div>
             </Dialog>
-        </Transition.Root>)
+        </Transition.Root>
+    );
 }
